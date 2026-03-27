8b7a001fe11cd597afd3e3b86c9b72732f147348
profLeoBarbosa <prof.lebc@gmail.com>
2026-03-26 21:25:55 -0300

    Add files via upload

---
+++ 26-03/exercicio5.js
@@ -0,0 +1,8 @@
+let nome = "Leonardo"
+let anoDeNascimento = 2001
+let anoAtual = 2026
+
+console.log("Nome:", nome)
+console.log("Idade:", anoAtual - anoDeNascimento)
+console.log("Maior de idade:", (anoAtual - anoDeNascimento >= 18))
+console.log("Em 2050", nome, "tera", 2050 - anoDeNascimento, "anos de idade")
